df6931bff8b11bae7e0953d9c5d5264b331760b6
Vitor Petrone <vitorhugo.sp@gmail.com>
2023-03-21 08:54:20 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Por favor, digite o CEP e pressione ENTER:");
+var cep = Console.ReadLine();
+
+string url = "https://viacep.com.br/ws/" + cep + "/json/";
+
+string result = new HttpClient().GetStringAsync(url).Result;
+
+Console.WriteLine(result);
+
